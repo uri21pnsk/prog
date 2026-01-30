39499186d41166d31e39cdf0c1b942228afde8fc
uri21pnsk <quilezgru@ieselaios.com>
2026-01-30 09:47:45 +0000

    ej 2

--- ejercicio 1.cpp
+++ ejercicio 1.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int=i;
+    int i;
     i=0;
     while(i<6){
 cout << "Hello Bebesita \n";
@@ -9,11 +9,11 @@ i=i+1;
     }
 }
 
-int main(){
+
 int=z;
 z=0;
 while (z<1298){
     cout<<"Te amo, eres  el mejor \n";
     z=z+1;
 }
-}
+
