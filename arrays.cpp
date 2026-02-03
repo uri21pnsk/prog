d8b6ecf1ece642c0be0d784b25314095e4fddce0
uri21pnsk <quilezgru@ieselaios.com>
2026-02-03 08:59:29 +0000

    ejercicio 2

---
+++ arrays.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<< "Dime cuantos números quieres sumar: ";
+    cin>> num;
+    cout<< "He entendido que quieres suamar " << num << " números \n";
+    int i;
+    i=0;
+    while (i<num){
+        int num;
+        cout<< "Dame cuantos números quierwes sumar \n";
+        cin>> num;
+        cout<< "He entendido que quieres sumar " << num << " números \n";
+        int i;
+        i=0;
+        while (i<num){
+            int valor;
+            cout<< "Dame un número: " <<i << "\n";
+            cin>> valores [i];
+            i=i+1;
+        }
+        int j=0;
+        while (j<num){
+            cout <<
+            
