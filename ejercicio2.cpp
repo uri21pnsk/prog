c7ccb555f2959448f48aeb563272aedfc0456452
uri21pnsk <quilezgru@ieselaios.com>
2026-02-06 09:54:48 +0000

    array

---
+++ ejercicio2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<< "Dime cuantos números quieres: ";
+    cin>> num; // el usuario me ha dado el número de dígitos y lo he metido en la variable num
+    cout << "muchas gracias, ahora se que me vas a dar " << num <<" numeros\n";
+
+    int i; 
+    i=num;
+    cout <<"ahora dime el primer número\n";///estamos pidiendo el primer número al usuario
+    cin >> i; ///el usuario ha justificado que su primer numero es el i
+    int k;
+    k=num;
+    cout << "muchas gracias, ahora se que el primer número es " << i <<".¿Ahora dime el segundo numero\n";
+    cin>>k; ///el usuario ha justificado que su segundo numero es el k
+    cout<<"Gracias de nuevo, ahora se que el segundo numero es "<< k <<".¿por favor dime el tercer número?\n";
+    int s
+
+    
+    
+    }
+
+
+
